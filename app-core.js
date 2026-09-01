e645247e793ac61eebba0a79b5e3c847e5400ef3
Bernio <jendabernard@gmail.com>
2026-09-01 17:40:48 +0200

    Wire core timetable scripts

---
+++ app-core.js
@@ -0,0 +1,5 @@
+document.write('<script src="app-core-1.js"><\/script>');
+document.write('<script src="app-core-2.js"><\/script>');
+document.write('<script src="app-core-3.js"><\/script>');
+document.write('<script src="app-core-4.js"><\/script>');
+document.write('<script src="app-core-5.js"><\/script>');
